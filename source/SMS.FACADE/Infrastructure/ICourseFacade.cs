245989ac273d1de1a5c28fab02a94ad8743fb831
M Umair Asim <mumairasim@hotmail.com>
2020-04-26 15:04:12 +0500

    Cousre Work Flow including Services,Facade and Api

---
+++ source/SMS.FACADE/Infrastructure/ICourseFacade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.FACADE.Infrastructure
+{
+    public interface ICourseFacade
+    {
+        string Test();
+    }
+}
+
---
+++ source/SMS.Services/Implementation/CourseService.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using SMS.DATA.Infrastructure;
+using SMS.REQUESTDATA.Infrastructure;
+using SMS.Services.Infrastructure;
+using Course = SMS.DATA.Models.Course;
+using RequestCourse = SMS.REQUESTDATA.RequestModels.Course;
+
+namespace SMS.Services.Implementation
+{
+    public class CourseService : ICourseService
+    {
+        private readonly IRepository<Course> _repository;
+        private readonly IRequestRepository<RequestCourse> _requestRepository;
+        public CourseService(IRepository<Course> repository, IRequestRepository<RequestCourse> requestRepository)
+        {
+            _repository = repository;
+            _requestRepository = requestRepository;
+        }
+        public string Get()
+        {
+            var courseList = _repository.Get().ToList();
+            var courseRequestList = _requestRepository.Get().ToList();
+            return "Hello World";
+        }
+    }
+}
