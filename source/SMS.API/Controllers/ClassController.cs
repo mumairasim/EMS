f03ea10b14f02c8b098ff5cf79f7dd9876546e70
shahab lodhi <shahablodhi93@outlook.com>
2020-05-05 05:39:22 +0500

    dependancy error fix

--- source/SMS.API/Controllers/ClassController.cs
+++ source/SMS.API/Controllers/ClassController.cs
@@ -2,49 +2,50 @@
 using SMS.FACADE.Infrastructure;
 using System.Web.Http;
 using DTOClass = SMS.DTOs.DTOs.Class;
+using SMS.Services.Infrastructure;
 
 namespace SMS.API.Controllers
 {
     [RoutePrefix("api/v1/Class")]
     public class ClassController : ApiController
     {
-        public IClassFacade _classFacade;
-        public ClassController(IClassFacade classFacade)
+        public IClassService _classService;
+        public ClassController(IClassService classService)
         {
-            _classFacade = classFacade;
+            _classService = classService;
         }
 
         [HttpGet]
         [Route("Get")]
         public IHttpActionResult Get()
         {
-            return Ok(_classFacade.Get());
+            return Ok(_classService.Get());
         }
         [HttpGet]
         [Route("Get")]
         public IHttpActionResult Get(Guid id)
         {
-            return Ok(_classFacade.Get(id));
+            return Ok(_classService.Get(id));
         }
         [HttpPost]
         [Route("Create")]
         public IHttpActionResult Create(DTOClass dtoClass)
         {
-            _classFacade.Create(dtoClass);
+            _classService.Create(dtoClass);
             return Ok();
         }
         [HttpPut]
         [Route("Update")]
         public IHttpActionResult Update(DTOClass dtoClass)
         {
-            _classFacade.Update(dtoClass);
+            _classService.Update(dtoClass);
             return Ok();
         }
         [HttpDelete]
         [Route("Delete")]
         public IHttpActionResult Delete(Guid id)
         {
-            _classFacade.Delete(id);
+            _classService.Delete(id);
             return Ok();
         }
     }
