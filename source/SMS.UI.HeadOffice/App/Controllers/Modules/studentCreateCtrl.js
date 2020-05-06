4df29ed8ee8069a422b7a6b97ae9b12870c8bedc
M Umair Asim <mumairasim@hotmail.com>
2020-05-06 16:21:16 +0500

    student create functionality added

--- source/SMS.UI.HeadOffice/App/Controllers/Modules/studentCreateCtrl.js
+++ source/SMS.UI.HeadOffice/App/Controllers/Modules/studentCreateCtrl.js
@@ -1,4 +1,59 @@
 ﻿SMSHO.controller('studentCreateCtrl', ['$scope', 'apiService', '$cookies', function ($scope, apiService, $cookies) {
     'use strict';
-
+    $scope.StudentModel = {
+        RegistrationNumber: '',
+        Person: $scope.Person,
+        Class: $scope.Class,
+        School: $scope.School
+    };
+    $scope.Person = {
+        FirstName: '',
+        LastName: '',
+        Cnic: '',
+        Nationality: '',
+        Religion: '',
+        PresentAddress: '',
+        PermanentAddress: '',
+        Phone: ''
+    };
+    $scope.Class = {
+        Id: '',
+        ClassName: ''
+    };
+    $scope.School = {
+        Id: '',
+        Name: '',
+        Location: ''
+    };
+    $scope.GetClasses = function () {
+        var responsedata = apiService.masterget('/api/v1/Class/Get');
+        responsedata.then(function mySucces(response) {
+            $scope.Classes = response.data;
+        },
+            function myError(response) {
+                $scope.response = response.data;
+            });
+    };
+    $scope.GetSchools = function () {
+        var responsedata = apiService.masterget('/api/v1/School/Get');
+        responsedata.then(function mySucces(response) {
+            $scope.Schools = response.data;
+        },
+            function myError(response) {
+                $scope.response = response.data;
+            });
+    };
+    $scope.StudentCreate = function () {
+        var data = $scope.StudentModel;
+        var responsedata = apiService.register('/api/v1/Student/Create', data);
+        responsedata.then(function mySucces(response) {
+            $scope.response = response.data;
+            $scope.growltext("Student created successfully.", false);
+            window.location = "#!/dashboard";
+        },
+            function myError(response) {
+                $scope.response = response.data;
+                $scope.growltext("Student creation failed", true);
+            });
+    };
 }]);
